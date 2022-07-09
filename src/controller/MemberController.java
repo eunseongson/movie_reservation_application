6e5086a381b66795691f2663a66003968d278362
daeunchung <jul12230103@gmail.com>
2022-07-09 17:20:43 +0900

    Feat : member logics 07-09

--- src/controller/MemberController.java
+++ src/controller/MemberController.java
@@ -1,12 +1,15 @@
 package controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
 
 import dao.MemberDao;
@@ -33,8 +36,10 @@ public class MemberController extends HttpServlet {
 
 		if (param.equals("login")) { // member?param=login
 			resp.sendRedirect("member/login.jsp");
+			
 		} else if (param.equals("regi")) {
 			resp.sendRedirect("member/regi.jsp");
+			
 		} else if (param.equals("logout")) {
 			String msg = "logout";
 			req.getSession().invalidate();
@@ -42,6 +47,7 @@ public class MemberController extends HttpServlet {
 
 		} else if (param.equals("withdraw")) {
 			resp.sendRedirect("member/withdraw.jsp");
+			
 		} else if (param.equals("idcheck")) {
 			String id = req.getParameter("id");
 			System.out.println("id:" + id);
@@ -60,6 +66,8 @@ public class MemberController extends HttpServlet {
 
 			resp.setContentType("application/x-json; charset=utf-8");
 			resp.getWriter().print(obj);
+			
+			
 		} else if (param.equals("regiAf")) {
 
 			String id = req.getParameter("id");
@@ -77,6 +85,7 @@ public class MemberController extends HttpServlet {
 			}
 			System.out.println(msg);
 			resp.sendRedirect("message.jsp?msg=" + msg);
+			
 		} else if (param.equals("loginAf")) {
 
 			String id = req.getParameter("id");
@@ -90,16 +99,18 @@ public class MemberController extends HttpServlet {
 			if (dto != null) { // 로그인 성공
 				req.getSession().setAttribute("login", dto);
 				msg = "loginOK";
-				// System.out.println(req.getSession().getId());
-				// resp.sendRedirect("index.jsp");
-			} else { // 그런 회원 없음
+			} else { 			// 로그인 실패
 				msg = "loginNO";
 
 			}
 			resp.sendRedirect("message.jsp?msg=" + msg);
 
 		} else if (param.equals("withdrawAf")) {
+			
 			String msg = "withdrawOK";
+			MemberDto dto = (MemberDto)req.getSession().getAttribute("login");
+			String withdrawId = dto.getId();
+			
 			req.getSession().invalidate();
 
 			// 진짜 탈퇴할꺼면 비번 누르고 탈퇴 승인
@@ -118,7 +129,72 @@ public class MemberController extends HttpServlet {
 			String id = req.getParameter("id");
 
 			resp.sendRedirect("member/mypage.jsp?id=" + id);
-		}
+			
+		} else if(param.equals("findidAf")) {
+			
+			String name = req.getParameter("name");
+			String email = req.getParameter("email");
 
+			MemberDao dao = MemberDao.getInstance();
+			MemberDto dto = dao.findid(new MemberDto(null, null, name, email, null, 0));
+
+			//String msg = "";
+			String str = "";
+
+			if (dto != null) { // 회원 id 있음
+				// url (get) 으로 안넘기고 post 로 param 여러개 넘기는 방법은 이거 또는 flashAttribute 뿐
+				// resp.getSession().setAttribute("isUser", dto);
+				// msg = "findidOK";
+				
+				str = "YES";
+			} else { 			// 회원 id 없음
+				// msg = "findidNO";
+				str = "NO";
+			}
+			// resp.sendRedirect("message.jsp?msg=" + msg);
+			
+
+			// 보내는 JSONObject : findid.jsp 에서 받는 Json
+			JSONObject obj = new JSONObject();
+			obj.put("msg", str); // 짐싸!
+			obj.put("name", name);
+			if(dto != null) {
+				obj.put("id", dto.getId());
+			}
+
+			resp.setContentType("application/x-json; charset=utf-8");
+			resp.getWriter().print(obj);
+
+			
+		} else if(param.equals("findpwAf")) {
+			String msg = "findpwOK";
+
+			String id = req.getParameter("id");
+			String email = req.getParameter("email");
+			String pw = "";
+			MemberDao dao = MemberDao.getInstance();
+			MemberDto dto = dao.findpw(new MemberDto(id, null, null, email, null, 0));
+			
+			//String msg = "";
+			String str = "";
+
+			if (dto != null) { // 회원 있음
+				str = "YES";
+			} else { 			// 회원 없음
+				str = "NO";
+			}
+			
+			// 보내는 JSONObject : findpw.jsp 에서 받는 Json
+			JSONObject obj = new JSONObject();
+			obj.put("msg", str); // 짐싸!
+			if(dto != null) {
+				obj.put("name", dto.getName());
+				obj.put("id", dto.getId());
+			}
+
+			resp.setContentType("application/x-json; charset=utf-8");
+			resp.getWriter().print(obj);
+
+		}
 	}
 }
