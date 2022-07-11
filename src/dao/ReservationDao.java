9938d2bcde4e03eca38956a47fb7580cc70ceb3f
SSOsh <tngh147258@kumoh.ac.kr>
2022-07-11 14:21:55 +0900

    Feat : modify movieStillCut 예외잡기

--- src/dao/ReservationDao.java
+++ src/dao/ReservationDao.java
@@ -1,21 +1,12 @@
 package dao;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import db.DBClose;
 import db.DBConnection;
-import dto.MemberDto;
 
 
 public class ReservationDao{
