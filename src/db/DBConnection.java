bfcb6f35d593bdd549cd347041ca5cbdeb853d5a
SSOsh <tngh147258@kumoh.ac.kr>
2022-07-11 09:02:51 +0900

    Feat : update DBConnection password

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -19,7 +19,7 @@ public class DBConnection {
 		
 		String url = "jdbc:mysql://localhost:3306/movie_reservation_db";
 		String user = "root";
-		String password = "zx12qw34";
+		String password = "1234";
 		
 		Connection conn = null;
 
